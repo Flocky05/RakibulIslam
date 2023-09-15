c2f9f696e2480a81bff9a9485f7abcaa90008285
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-15 10:37:01 +0600

    update home page

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
         window.open(url);
     }
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center pt-15">
-            <div className="text-center p-10 flex flex-col items-center">
+        <div className="flex justify-around items-center pt-15">
+            <div className="p-10 flex flex-col items-center w-2/4">
                 <h2 className="text-2xl font-bold">Hi,This Rakibul Islam</h2>
                 <p className="my-2">I’m a Web Developer and has been learning web developing more than 1 year now. I am studying B.Sc in Computer Science and Engineering in Faridpur Engineering College.</p>
                 <p className="my-5">With the ability to learn new things and adapt in both self-starting and collaborative environments while staying focused on achieving high-quality results under strict deadlines. Eager to obtain a challenging position at a prestigious company. I’m very much interested in new technologies and problem solving.</p>
@@ -14,8 +14,8 @@ const Home = () => {
                     Resume
                 </button>
             </div>
-            <div>
-                <img className="w-full h-[400px] object-cover rounded-full " src={img} alt="" />
+            <div className="w-2/4 flex flex-col items-center ">
+                <img className="w-2/4 h-[370px] rounded-full border-2" src={img} alt="" />
             </div>
         </div>
     );
