e6ffd60f23f57a1679ae005f076e19bf885f7199
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-26 20:27:09 +0600

    update home page

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,7 +3,7 @@
 const Header = () => {
     return (
         <div className="flex justify-around p-10 text-white text-xl font-semibold">
-            <a href="/"> <span className="text-blue-500">Rakibul</span> Islam </a>
+            <a className="text-2xl" href="/"> <span className="text-blue-500">Rakibul</span> Islam </a>
             <div className="grid grid-cols-5 w-2/4">
                 <a href="/about">About</a>
                 <a href="/resume">Resume</a>
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -5,17 +5,17 @@ const Home = () => {
         window.open(url);
     }
     return (
-        <div className="flex justify-around items-center pt-15">
-            <div className="p-10 flex flex-col items-center w-2/4">
+        <div className="lg:flex grid grid-cols-1 justify-around items-center pt-15 h-screen">
+            <div className="p-10 justify-items-center text-center lg:text-start lg:w-2/4">
                 <h2 className="text-2xl font-bold">Hi,This Rakibul Islam</h2>
                 <p className="my-2">I’m a Web Developer and has been learning web developing more than 1 year now. I am studying B.Sc in Computer Science and Engineering in Faridpur Engineering College.</p>
                 <p className="my-5">With the ability to learn new things and adapt in both self-starting and collaborative environments while staying focused on achieving high-quality results under strict deadlines. Eager to obtain a challenging position at a prestigious company. I’m very much interested in new technologies and problem solving.</p>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg border-white" onClick={()=>newTab('https://drive.google.com/file/d/1GXtvXuihnkRckEdls9hxbQnFCLkQKQT8/view')}>
+                <button className="bg-blue-500 text-white px-6 py-2 rounded-lg border-white" onClick={()=>newTab('https://drive.google.com/file/d/1GXtvXuihnkRckEdls9hxbQnFCLkQKQT8/view')}>
                     Resume
                 </button>
             </div>
-            <div className="w-2/4 flex flex-col items-center ">
-                <img className="w-2/4 h-[370px] rounded-full border-2" src={img} alt="" />
+            <div className="lg:w-2/4 flex flex-col items-center ">
+                <img className="w-2/4 h-[370px] rounded-full border-4 border-indigo-600" src={img} alt="" />
             </div>
         </div>
     );
