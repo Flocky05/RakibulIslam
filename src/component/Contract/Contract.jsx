449da606138f8e1fbadd19aedd6c1b5f89bd5a0b
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-27 12:51:08 +0600

    update contact section

--- src/component/Contract/Contract.jsx
+++ src/component/Contract/Contract.jsx
@@ -3,10 +3,19 @@
 const Contract = () => {
     return (
         <div>
-            <h2 className="text-2xl font-bold text-center p-8 uppercase">COntact</h2>
-            <form action="">
-                
-            </form>
+            <h2 className="text-2xl font-bold text-center p-8 uppercase">Let's Contact</h2>
+            <div className="p-12 text-center">
+                <div>
+                    <div>
+                        <input className="w-3/5 py-3 rounded-lg my-2 px-4 hover:shadow-lg  hover:shadow-gray-400 duration-700 ease-in-out" type="text" placeholder="Name"/>
+                        <input className="w-3/5 py-3 rounded-lg my-2 px-4 hover:shadow-lg  hover:shadow-gray-400 duration-700 ease-in-out" type="email" placeholder="Email"/>
+                    </div>
+                    <input className="w-3/5 py-3 rounded-lg my-2 px-4 hover:shadow-lg hover:shadow-gray-400 duration-700 ease-in-out" type="text" placeholder="subject" />
+                    <textarea className="w-3/5 py-3 rounded-lg my-2 px-4 hover:shadow-lg hover:shadow-gray-400 duration-700 ease-in-out" name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                    <br />
+                    <button className="bg-gray-500 text-white w-3/5 hover:shadow-lg hover:shadow-gray-400 duration-700 ease-in-out">send</button>
+                    </div>
+            </div>
         </div>
     );
 };
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -10,12 +10,12 @@ const Home = () => {
                 <h2 className="text-2xl font-bold">Hi,This Rakibul Islam</h2>
                 <p className="my-2">I’m a Web Developer and has been learning web developing more than 1 year now. I am completed B.Sc in Computer Science and Engineering at Faridpur Engineering College.</p>
                 <p className="my-5">With the ability to learn new things and adapt in both self-starting and collaborative environments while staying focused on achieving high-quality results under strict deadlines. Eager to obtain a challenging position at a prestigious company. I’m very much interested in new technologies and problem solving.</p>
-                <button className="bg-blue-500 text-white px-6 py-2 rounded-lg border-white" onClick={()=>newTab('https://drive.google.com/file/d/1GXtvXuihnkRckEdls9hxbQnFCLkQKQT8/view')}>
+                <button className="bg-gray-500 text-white px-6 py-2 rounded-lg hover:shadow-lg shadow-blue-500/40 hover:shadow-gray-400 duration-700 ease-in-out" onClick={()=>newTab('https://drive.google.com/file/d/1GXtvXuihnkRckEdls9hxbQnFCLkQKQT8/view')}>
                     Resume
                 </button>
             </div>
             <div className="lg:w-2/4 flex flex-col items-center ">
-                <img className="w-2/4 h-[370px] rounded-full border-4 border-indigo-600" src={img} alt="" />
+                <img className="w-2/4 h-[370px] rounded-full border-4 border-gray-400 hover:shadow-xl shadow-blue-500/40 hover:shadow-gray-400 duration-700 ease-in-out" src={img} alt="" />
             </div>
         </div>
     );
