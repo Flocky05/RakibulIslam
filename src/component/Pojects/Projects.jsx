cfa829ba1aa85d6cb2aadf924d1fbb1676015b1e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-11-12 23:05:19 +0600

    added project section

--- src/component/Pojects/Projects.jsx
+++ src/component/Pojects/Projects.jsx
@@ -1,24 +1,10 @@
-import Carousel from "./Carousel"
 
-const slides = [
-    "./1.jpg",
-    "./2.jpg",
-    "./3.jpg",
-    "./4.jpg",
-  ]
 
 const Projects = () => {
     return (
-        <div className="flex justify-center items-center h-screen bg-black">
-      <div className="max-w-lg">
-        <Carousel autoSlide={true} >
-          {[...slides.map((s) => (
-            <img src={s} />
-          )), <video src={vid} autoPlay muted loop />]}
-        </Carousel>
-
-      </div>
-    </div>
+        <div>
+            <h2>This is Project section</h2>
+        </div>
     );
 };
 
