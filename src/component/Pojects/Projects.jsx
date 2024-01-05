b42938116e98e3132ab93504243d46f284ef23bc
Flocky05 <md.rakibul.islam.0501@gmail.com>
2024-01-05 23:44:15 +0600

    added project section

--- src/component/Pojects/Projects.jsx
+++ src/component/Pojects/Projects.jsx
@@ -1,9 +1,15 @@
-
+import img1 from "../../assets/projects/gpt3/bike-down.png"
+import img2 from "../../assets/projects/gpt3/coocky.png"
+import img3 from "../../assets/projects/gpt3/e-commerce-movie.png"
+import img4 from "../../assets/projects/gpt3/ganimy.png"
 
 const Projects = () => {
     return (
-        <div>
-            <h2>This is Project section</h2>
+        <div className="grid grid-cols-3 gap-20 m-10">
+            <img src={img1} alt="" />
+            <img src={img2} alt="" />
+            <img src={img3} alt="" />
+            <img src={img4} alt="" />
         </div>
     );
 };
