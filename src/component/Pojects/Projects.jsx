db914a3f9934e84b6dd4c882ad9769a51bf3602c
Flocky05 <md.rakibul.islam.0501@gmail.com>
2024-01-06 00:04:42 +0600

    added projects contantent

--- src/component/Pojects/Projects.jsx
+++ src/component/Pojects/Projects.jsx
@@ -6,7 +6,14 @@ import img4 from "../../assets/projects/gpt3/ganimy.png"
 const Projects = () => {
     return (
         <div className="grid grid-cols-3 gap-20 m-10">
-            <img src={img1} alt="" />
+            
+            <div className="p-4 border border-blue-400 rounded-lg bg-blue-100">
+                <img className="h-full" src={img1} alt="" />
+                <div className=" flex justify-between">
+                    <p className="border border-red-200 px-2 bg-green-400">Github</p>
+                    <p>Code</p>
+                </div>
+            </div>
             <img src={img2} alt="" />
             <img src={img3} alt="" />
             <img src={img4} alt="" />
