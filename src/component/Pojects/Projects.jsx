9ddc59199befd01c19045c9223750aad77ba66d6
Flocky05 <md.rakibul.islam.0501@gmail.com>
2024-01-06 23:17:59 +0600

    update profile section and added scrol bar

--- src/component/Pojects/Projects.jsx
+++ src/component/Pojects/Projects.jsx
@@ -2,21 +2,126 @@ import img1 from "../../assets/projects/gpt3/bike-down.png"
 import img2 from "../../assets/projects/gpt3/coocky.png"
 import img3 from "../../assets/projects/gpt3/e-commerce-movie.png"
 import img4 from "../../assets/projects/gpt3/ganimy.png"
+import img5 from "../../assets/projects/gpt3/gpt-3-demo.netlify.app_(Nest Hub Max).png"
+import img6 from "../../assets/projects/gpt3/innox-product.png"
+import img7 from "../../assets/projects/gpt3/job-finer-demo01.png"
+import img8 from "../../assets/projects/gpt3/programmer.png"
+import img10 from "../../assets/projects/gpt3/rakibs-photography.png"
+import img11 from "../../assets/projects/gpt3/rohan-television.png"
+import img12 from "../../assets/projects/gpt3/tea-house.png"
+import img13 from "../../assets/projects/gpt3/travel-tuki-taki.png"
+
+// const images=[img1,img2,img3,img4,img5,img6,img7,img8,img10,img11,img12,img13];
 
 const Projects = () => {
     return (
-        <div className="grid grid-cols-3 gap-20 m-10">
-            
-            <div className="p-4 border border-blue-400 rounded-lg bg-blue-100">
-                <img className="h-full" src={img1} alt="" />
-                <div className=" flex justify-between">
-                    <p className="border border-red-200 px-2 bg-green-400">Github</p>
-                    <p>Code</p>
+        <div className="absolute grid grid-cols-3 gap-20 m-10 bg-black">
+            <div>
+                <div className="relative overflow-hidden rounded-lg">
+                    <img className=" object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img1} alt="" />
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe nam, repellendus voluptatem optio voluptates facilis quam natus dolorem quis esse.</p>
+                <button className="bg-blue-600 px-4 py-2 rounded-md">More</button>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img2} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
                 </div>
             </div>
-            <img src={img2} alt="" />
-            <img src={img3} alt="" />
-            <img src={img4} alt="" />
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img3} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img4} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img5} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img6} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img7} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img8} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img10} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img11} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img12} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-red-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
+            <div>
+                <div className="overflow-hidden rounded-lg">
+                    <img className="object-cover object-top hover:object-bottom h-80 w-full ease-out duration-1000" src={img13} alt="" />
+                </div>
+                <div className="grid grid-cols-2 justify-between text-white p-4 gap-4">
+                    <p className="bg-green-600 px-4 py-2 rounded-md">live</p>
+                    <p className="bg-blue-300 px-4 py-2 rounded-md">Github</p>
+                </div>
+            </div>
         </div>
     );
 };
